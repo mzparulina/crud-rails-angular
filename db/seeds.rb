113c4dd9aa532d260e9b31ff5e8a200318d5dee1
mzparulina <pauline.revilla@gmail.com>
2023-07-01 09:39:16 -0400

    Add data on user db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'Luke Wan', age: 23, address: '123 Test St.')
+User.create(name: 'Mary Poppins', age: 41, address: '123 ABC St.')
+User.create(name: 'John Neilman', age: 76, address: '000 Test St.')
